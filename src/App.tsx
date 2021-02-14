32341b2f433cb87bfadfbf504da5400b255361cd
Alex Karo <alex@karo-dev.ru>
2021-02-14 21:33:47 +0300

    Split phases, not months

--- src/App.tsx
+++ src/App.tsx
@@ -19,54 +19,71 @@ const colors = [
   'darkmagenta',
 ];
 
-function getDayName(count: number): string {
-  if (count === 1) {
-    return ' (New Moon)';
-  }
-  if (count === 15) {
-    return ' (Full Moon)';
-  }
-  return '';
+const MOON_PHASES = {
+  0: "🌑 New Moon",
+  1: "🌓 First Quarter",
+  2: "🌕 Full Moon",
+  3: "🌗 Last Quarter",
+};
+
+async function getMoonPhases(year: number): Promise<Array<{ Date: string, Phase: 0 | 1 | 2 | 3 }>> {
+  const res = await fetch(
+    `https://raw.githubusercontent.com/CraigChamberlain/moon-data/master/api/moon-phase-data/${year}/index.json`,
+  );
+  return await res.json();
 }
 
 function App() {
-  const [dates, setDates] = useState<Day[] | null>(null);
+  const [days, setDays] = useState<Day[] | null>(null);
+  const [phases, setPhases] = useState<Array<{ Date: string, Phase: 0 | 1 | 2 | 3 }> | null>(null);
   useEffect(() => {
     (async () => {
-      const res = await fetch('https://raw.githubusercontent.com/CraigChamberlain/moon-data/master/api/new-moon-data/2021/index.json');
-      const data = await res.json();
+      const data = await getMoonPhases(2021);
+      setPhases(data);
       const days: Day[] = [];
       for (let i = 0; i < data.length - 1; i += 1) {
-        const prevMoon = new Date(data[i]).getTime();
-        const nextMoon = new Date(data[i + 1]).getTime();
-        const dayDuration = Math.floor((nextMoon - prevMoon) / 28);
-        days.push(...Array(28).fill(null).map((_, i) => ({
-          start: new Date(prevMoon + dayDuration * i),
-          end: new Date(prevMoon + dayDuration * (i + 1)),
-          count: i + 1,
+        const phaseCount = data[i].Phase;
+        const prevPhaseDate = new Date(data[i].Date).getTime();
+        const nextPhaseDate = new Date(data[i + 1].Date).getTime();
+        const dayDuration = Math.floor((nextPhaseDate - prevPhaseDate) / 7);
+        days.push(...Array(7).fill(null).map((_, i) => ({
+          start: new Date(prevPhaseDate + dayDuration * i),
+          end: new Date(prevPhaseDate + dayDuration * (i + 1)),
+          count: phaseCount * 7 + i + 1 ,
         })));
       }
-      setDates(days);
+      setDays(days);
     })();
   }, [])
 
-  if (dates === null) {
+  if (days === null || phases === null) {
     return null;
   }
+
+  const dayEvents = days.map(({start, end, count}) => ({
+    start,
+    end,
+    title: `Day ${count}`,
+    display: 'background',
+    backgroundColor: colors[(count - 1) % 7],
+    textColor: 'black',
+  }));
+
+  const phaseEvents = phases.map(({Date, Phase}) => ({
+    start: Date,
+    end: Date,
+    title: MOON_PHASES[Phase],
+  }));
 
   return (
     <div className="App">
       <FullCalendar
         plugins={[ timeGridWeek ]}
         initialView="timeGridWeek"
-        events={dates.map(({start, end, count}) => ({
-          start,
-          end,
-          title: `Day ${count}${getDayName(count)}`,
-          display: 'background',
-          backgroundColor: colors[(count - 1) % 7],
-          textColor: 'black',
-        }))}
+        events={[
+          ...dayEvents,
+          ...phaseEvents,
+        ]}
         slotDuration={{ hours: 1 }}
         allDaySlot={false}
         height={'auto'}
